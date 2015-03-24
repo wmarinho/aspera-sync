2ae2b8636110825704cfadbba602aafbb73386b5
Wellington Marinho <wpmarinho@globo.com>
2015-03-24 16:00:30 +0000

    Added README.md

--- bin/init-jenkins.sh
+++ bin/init-jenkins.sh
@@ -2,4 +2,7 @@
 if [ ! -d "$HOME/aspera-sync/logs" ]; then
   mkdir -p $HOME/aspera-sync/logs
 fi
-nohup java -jar $HOME/aspera-sync/jenkins/jenkins.war >  $HOME/aspera-sync/logs/jenkins.log 2>&1
+if [ "$1" ]; then
+  JENKINS_OPTIONS=$1
+fi
+nohup java -jar $HOME/aspera-sync/jenkins/jenkins.war ${JENKINS_OPTIONS} >  $HOME/aspera-sync/logs/jenkins.log 2>&1
--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo sh bin/install-ascp.sh
-sh bin/install-jenkins.sh
